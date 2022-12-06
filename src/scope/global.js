b9fbabb226ed2e8b0a67616192f9da3fc9189f53
PepeCharly01 <jc.espinosa2@gmail.com>
2022-12-05 20:57:28 -0600

    primer ejercicio de scope global

---
+++ src/scope/global.js
@@ -0,0 +1,21 @@
+// Variables
+var a; // declarando
+var b = 'b'; // declarando y asignamos
+b = 'bb'; // reasignacion
+var a = 'aa'; //redeclaracion
+
+// Global scope
+var fruta = 'manzana'; // global
+console.log(fruta);
+
+function frutaFavorita() {
+    console.log(fruta);
+}
+frutaFavorita();
+
+function paises () {
+    pais = 'Mexico';
+    console.log(pais);
+}
+paises();
+console.log(pais);
