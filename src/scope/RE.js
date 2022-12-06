39823a1235d04bf82842b01cb4ca38ff7506c47e
PepeCharly01 <jc.espinosa2@gmail.com>
2022-12-05 21:42:14 -0600

    ejercicio de reasignacion y redeclaracion

---
+++ src/scope/RE.js
@@ -0,0 +1,27 @@
+var primerNombre; // Undefined
+primerNombre = 'Carlos';
+console.log(primerNombre);
+
+var segundoNombre = "David"; // declarar y asignar
+segundoNombre = 'Ana'; // reasignar
+console.log(segundoNombre);
+
+var apellido = 'Flores'; // declarar y asignar
+var apellido = 'Lopez'; // redeclarar
+console.log(apellido);
+
+// Let
+let fruit = 'Apple'; // declarar y asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+// let fruit = 'Banana'; // no se puede reedeclarar
+// console.log(fruit);
+
+// const
+const animal = 'dog'; // declara y asigna
+// animal = 'cat'; // reasignando
+console.log(animal);
+
+const veiculos = [];
+veiculos.push("🚗");
